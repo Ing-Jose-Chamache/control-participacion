0876e061d122890da751d290148aac2e039c3921
Ing-Jose-Chamache <jchamache@senati.pe>
2025-03-03 09:05:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -416,7 +416,7 @@ st.markdown("""
     /* Estilos para el menú de IA */
     .ia-menu {
         position: fixed !important;
-        top: 20px !important;
+        top: 50px !important;
         left: 50% !important;
         transform: translateX(-50%) !important;
         z-index: 9999 !important;
